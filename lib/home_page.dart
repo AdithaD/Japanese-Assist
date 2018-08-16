ed291406b798ff70a6d3082b1bb09bc639f5f472
indegame <indegamedev@gmail.com>
2018-08-17 09:29:16 +1000

    Created basic UI Framework (non functional) Add XML import

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class HomePage extends StatefulWidget{
+
+  createState() => new HomePageState();
+
+}
+
+class HomePageState extends State<HomePage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+
+    );
+  }
+
+  Widget _createHomePageBody(){
+
+  }
+}
